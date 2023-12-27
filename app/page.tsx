c83d3c169bc1604a013774c844dc54c43a7d544c
emre1n <emre1n@protonmail.com>
2023-12-27 23:19:31 +0300

    add docker-compose file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Drizzle ORM with Postgres DB</h1>
+    <main className="bg-black h-screen p-8">
+      <h1 className="text-lime-500 text-center">
+        Drizzle ORM with Postgres DB
+      </h1>
     </main>
   );
 }
