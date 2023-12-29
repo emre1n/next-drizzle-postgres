5dafb1f5c5b81ab915eaeaf912e94180be8c8c09
emre1n <emre1n@protonmail.com>
2023-12-29 23:15:29 +0300

    add SQL types part 1

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,34 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import {
+  bigint,
+  bigserial,
+  boolean,
+  char,
+  decimal,
+  doublePrecision,
+  json,
+  jsonb,
+  pgTable,
+  serial,
+  text,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   fullName: text('full_name'),
   phone: varchar('phone', { length: 256 }),
   address: varchar('address', { length: 256 }),
+});
+
+export const testTable = pgTable('testTable', {
+  id: bigserial('id', { mode: 'bigint' }).primaryKey(),
+  qty: bigint('qty', { mode: 'bigint' }),
+  price: decimal('price', { precision: 7, scale: 2 }), // 12345.67
+  score: doublePrecision('score'),
+  delivered: boolean('delivered'),
+  // description: text('description'),
+  description: varchar('description', { length: 256 }),
+  name: char('name', { length: 10 }), // "chair     " - 10 a string of characters total
+  // data: json('data'),
+  data: jsonb('data'), // with jsonb the js object will be converted to binary type
 });
---
+++ drizzle/0000_dear_hitman.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "testTable" (
+	"qty" integer
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"full_name" text,
+	"phone" varchar(256),
+	"address" varchar(256)
+);
