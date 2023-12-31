19fb263c4fe4cce285886e06ccb7d5c48fb8a20e
emre1n <emre1n@protonmail.com>
2023-12-31 23:08:47 +0300

    add profiles table & one-to-many relation with users table

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,35 @@
 import { db } from '@/db';
 import { users } from '@/db/schema';
-import { eq } from 'drizzle-orm';
+import {
+  and,
+  between,
+  eq,
+  gt,
+  gte,
+  ilike,
+  inArray,
+  isNotNull,
+  isNull,
+  like,
+  lt,
+  lte,
+  ne,
+  not,
+  notBetween,
+  notIlike,
+  notInArray,
+  notLike,
+  or,
+} from 'drizzle-orm';
 
 export async function GET() {
-  const result = await db.select().from(users).where(eq(users.id, 1));
+  // const result = await db.select().from(users).where(eq(users.id, 1));
+
+  const result = await db.query.users.findFirst({
+    with: {
+      profile: true,
+    },
+  });
+
   return new Response(JSON.stringify(result));
 }
--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from 'drizzle-orm';
 import {
   bigint,
   bigserial,
@@ -17,8 +18,6 @@ import {
   varchar,
 } from 'drizzle-orm/pg-core';
 
-export const moodEnum = pgEnum('mood', ['sad', 'ok', 'happy']);
-
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   fullName: text('full_name'),
@@ -27,19 +26,35 @@ export const users = pgTable('users', {
   score: integer('score'),
 });
 
-export const testTable = pgTable('testTable', {
-  id: bigserial('id', { mode: 'bigint' }).primaryKey(),
-  qty: bigint('qty', { mode: 'bigint' }),
-  price: decimal('price', { precision: 7, scale: 2 }), // 12345.67
-  score: doublePrecision('score'),
-  delivered: boolean('delivered'),
-  // description: text('description'),
-  description: varchar('description', { length: 256 }),
-  name: char('name', { length: 10 }), // "chair     " - 10 a string of characters total
-  // data: json('data'),
-  data: jsonb('data'), // with jsonb the js object will be converted to binary type
-  date: date('date', { mode: 'date' }).defaultNow(),
-  interval2: interval('interval2', { fields: 'day' }),
-  startAt: time('startAt', { precision: 0 }).defaultNow(),
-  mood: moodEnum('mood').default('ok'),
+export const userRelations = relations(users, ({ one }) => ({
+  profile: one(profiles, {
+    fields: [users.id],
+    references: [profiles.userId],
+  }),
+}));
+
+export const profiles = pgTable('profiles', {
+  id: serial('id').primaryKey(),
+  bio: varchar('bio', { length: 256 }),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id),
 });
+
+// export const moodEnum = pgEnum('mood', ['sad', 'ok', 'happy']);
+// export const testTable = pgTable('testTable', {
+//   id: bigserial('id', { mode: 'bigint' }).primaryKey(),
+//   qty: bigint('qty', { mode: 'bigint' }),
+//   price: decimal('price', { precision: 7, scale: 2 }), // 12345.67
+//   score: doublePrecision('score'),
+//   delivered: boolean('delivered'),
+//   // description: text('description'),
+//   description: varchar('description', { length: 256 }),
+//   name: char('name', { length: 10 }), // "chair     " - 10 a string of characters total
+//   // data: json('data'),
+//   data: jsonb('data'), // with jsonb the js object will be converted to binary type
+//   date: date('date', { mode: 'date' }).defaultNow(),
+//   interval2: interval('interval2', { fields: 'day' }),
+//   startAt: time('startAt', { precision: 0 }).defaultNow(),
+//   mood: moodEnum('mood').default('ok'),
+// });
