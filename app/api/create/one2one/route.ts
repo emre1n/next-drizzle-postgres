e64818b3df37df2be0173cd39a4a09ccd822329a
emre1n <emre1n@protonmail.com>
2024-01-03 22:35:12 +0300

    add create end point with one2one relation

---
+++ app/api/create/one2one/route.ts
@@ -0,0 +1,34 @@
+import { db } from '@/db';
+import { profiles, users } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+
+export async function POST() {
+  const newUsers = await db
+    .insert(users)
+    .values({
+      address: 'street 1',
+      fullName: 'user 1',
+      phone: '5554433',
+      score: 95,
+    })
+    .returning({
+      userId: users.id,
+    });
+
+  await db
+    .insert(profiles)
+    .values({
+      userId: newUsers[0].userId,
+      bio: 'I am a programmer',
+    })
+    .execute();
+
+  const result = await db.query.users.findFirst({
+    where: eq(users.id, newUsers[0].userId),
+    with: {
+      profile: true,
+    },
+  });
+
+  return new Response(JSON.stringify(result));
+}
