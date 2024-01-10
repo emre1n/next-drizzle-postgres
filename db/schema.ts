6b5cc19c14b14838a5a9da051b89331786b363db
emre1n <emre1n@protonmail.com>
2024-01-10 21:36:49 +0300

    update schema with auth tables

--- db/schema.ts
+++ db/schema.ts
@@ -22,102 +22,12 @@ import {
 
 import type { AdapterAccount } from '@auth/core/adapters';
 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
-  address: varchar('address', { length: 256 }),
-  score: integer('score'),
-});
-
-export const userRelations = relations(users, ({ one, many }) => ({
-  profile: one(profiles, {
-    fields: [users.id],
-    references: [profiles.userId],
-  }),
-  posts: many(posts),
-}));
-
-export const posts = pgTable('posts', {
-  id: serial('id').primaryKey(),
-  text: varchar('text', { length: 256 }),
-  authorId: integer('author_id')
-    .notNull()
-    .references(() => users.id),
-});
-
-export const postRelations = relations(posts, ({ one, many }) => ({
-  author: one(users, {
-    fields: [posts.authorId],
-    references: [users.id],
-  }),
-
-  postOnCategories: many(postOnCategories),
-}));
-
-export const categories = pgTable('categories', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 256 }),
-});
-
-export const categoryRelations = relations(categories, ({ many }) => ({
-  posts: many(postOnCategories),
-}));
-
-export const postOnCategories = pgTable(
-  'post_categories',
-  {
-    postId: integer('post_id')
-      .notNull()
-      .references(() => posts.id),
-    categoryId: integer('category_id')
-      .notNull()
-      .references(() => categories.id),
-  },
-  t => ({
-    pk: primaryKey(t.postId, t.categoryId),
-  })
-);
-
-export const postOnCategoriesRelations = relations(
-  postOnCategories,
-  ({ one }) => ({
-    post: one(posts, {
-      fields: [postOnCategories.postId],
-      references: [posts.id],
-    }),
-
-    category: one(categories, {
-      fields: [postOnCategories.categoryId],
-      references: [categories.id],
-    }),
-  })
-);
-
-export const profiles = pgTable('profiles', {
-  id: serial('id').primaryKey(),
-  bio: varchar('bio', { length: 256 }),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => users.id),
-});
-
-// export const moodEnum = pgEnum('mood', ['sad', 'ok', 'happy']);
-// export const testTable = pgTable('testTable', {
-//   id: bigserial('id', { mode: 'bigint' }).primaryKey(),
-//   qty: bigint('qty', { mode: 'bigint' }),
-//   price: decimal('price', { precision: 7, scale: 2 }), // 12345.67
-//   score: doublePrecision('score'),
-//   delivered: boolean('delivered'),
-//   // description: text('description'),
-//   description: varchar('description', { length: 256 }),
-//   name: char('name', { length: 10 }), // "chair     " - 10 a string of characters total
-//   // data: json('data'),
-//   data: jsonb('data'), // with jsonb the js object will be converted to binary type
-//   date: date('date', { mode: 'date' }).defaultNow(),
-//   interval2: interval('interval2', { fields: 'day' }),
-//   startAt: time('startAt', { precision: 0 }).defaultNow(),
-//   mood: moodEnum('mood').default('ok'),
+// export const users = pgTable('users', {
+//   id: serial('id').primaryKey(),
+//   fullName: text('full_name'),
+//   phone: varchar('phone', { length: 256 }),
+//   address: varchar('address', { length: 256 }),
+//   score: integer('score'),
 // });
 
 export const users = pgTable('user', {
@@ -171,3 +81,93 @@ export const verificationTokens = pgTable(
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   })
 );
+
+// export const userRelations = relations(users, ({ one, many }) => ({
+//   profile: one(profiles, {
+//     fields: [users.id],
+//     references: [profiles.userId],
+//   }),
+//   posts: many(posts),
+// }));
+
+// export const posts = pgTable('posts', {
+//   id: serial('id').primaryKey(),
+//   text: varchar('text', { length: 256 }),
+//   authorId: integer('author_id')
+//     .notNull()
+//     .references(() => users.id),
+// });
+
+// export const postRelations = relations(posts, ({ one, many }) => ({
+//   author: one(users, {
+//     fields: [posts.authorId],
+//     references: [users.id],
+//   }),
+
+//   postOnCategories: many(postOnCategories),
+// }));
+
+// export const categories = pgTable('categories', {
+//   id: serial('id').primaryKey(),
+//   name: varchar('name', { length: 256 }),
+// });
+
+// export const categoryRelations = relations(categories, ({ many }) => ({
+//   posts: many(postOnCategories),
+// }));
+
+// export const postOnCategories = pgTable(
+//   'post_categories',
+//   {
+//     postId: integer('post_id')
+//       .notNull()
+//       .references(() => posts.id),
+//     categoryId: integer('category_id')
+//       .notNull()
+//       .references(() => categories.id),
+//   },
+//   t => ({
+//     pk: primaryKey(t.postId, t.categoryId),
+//   })
+// );
+
+// export const postOnCategoriesRelations = relations(
+//   postOnCategories,
+//   ({ one }) => ({
+//     post: one(posts, {
+//       fields: [postOnCategories.postId],
+//       references: [posts.id],
+//     }),
+
+//     category: one(categories, {
+//       fields: [postOnCategories.categoryId],
+//       references: [categories.id],
+//     }),
+//   })
+// );
+
+// export const profiles = pgTable('profiles', {
+//   id: serial('id').primaryKey(),
+//   bio: varchar('bio', { length: 256 }),
+//   userId: integer('user_id')
+//     .notNull()
+//     .references(() => users.id),
+// });
+
+// export const moodEnum = pgEnum('mood', ['sad', 'ok', 'happy']);
+// export const testTable = pgTable('testTable', {
+//   id: bigserial('id', { mode: 'bigint' }).primaryKey(),
+//   qty: bigint('qty', { mode: 'bigint' }),
+//   price: decimal('price', { precision: 7, scale: 2 }), // 12345.67
+//   score: doublePrecision('score'),
+//   delivered: boolean('delivered'),
+//   // description: text('description'),
+//   description: varchar('description', { length: 256 }),
+//   name: char('name', { length: 10 }), // "chair     " - 10 a string of characters total
+//   // data: json('data'),
+//   data: jsonb('data'), // with jsonb the js object will be converted to binary type
+//   date: date('date', { mode: 'date' }).defaultNow(),
+//   interval2: interval('interval2', { fields: 'day' }),
+//   startAt: time('startAt', { precision: 0 }).defaultNow(),
+//   mood: moodEnum('mood').default('ok'),
+// });
